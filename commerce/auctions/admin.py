64cb8a5610d30612003600313a9288cb84787e39
He-Xinq <149992260+He-Xinq@users.noreply.github.com>
2023-12-11 11:08:05 +0800

    Update admin.py

--- commerce/auctions/admin.py
+++ commerce/auctions/admin.py
@@ -1,10 +1,33 @@
 from django.contrib import admin
-from .models import User, Category, Listing, Comment, Bid
-
-
+from .models import Bid, Auction, Comment, Category, User, Photo, WatchList
 # Register your models here.
+
+class AuctionAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "current_bid", "creation_date","available","winner","auction_category","creator")
+
+#class AuctionAdmin(admin.ModelAdmin):
+#    filter_horizontal = ("photos",)
+
+class BidAdmin(admin.ModelAdmin):
+    list_display = ("id","price","auction_bid","auctioner")
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("id","commenter","comment_to","text")
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id","name")
+
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = ("id","image_name","url")
+
+class WatchAdmin(admin.ModelAdmin):
+   filter_horizontal = ()
+
+
+admin.site.register(Bid,BidAdmin)
+admin.site.register(Auction,AuctionAdmin)
+admin.site.register(Comment,CommentAdmin)
+admin.site.register(Category,CategoryAdmin)
+admin.site.register(Photo,PhotoAdmin)
+admin.site.register(WatchList,WatchAdmin)
 admin.site.register(User)
-admin.site.register(Category)
-admin.site.register(Listing)
-admin.site.register(Comment)
-admin.site.register(Bid)
